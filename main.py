215d6ad750b06023ac18e3c1517084a0e3a5b4a6
Fabio Dantas Fernandes <nunseik@gmail.com>
2025-04-17 22:25:16 -0300

    csv basic implementation

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
 import requests
 import csv
+import os
 
 r = requests.get('https://www2.itanhaem.sp.gov.br/wp-json/wp/v2/posts', timeout=1)
 
 if r.status_code != requests.codes.ok:
     raise Exception("Servidor indisponivel, tente novamente.")
 
-print(r.json()[0].keys())
+if os.path.exists("news.csv"):
 
-# with open('news.csv', newline='') as csvfile:
-#     reader = csv.DictReader(csvfile)
-#     first_row = next(reader)
-#     print(int(first_row['id']))
+    with open('last_id.txt', 'r') as txtfile:
+        last_id = int(txtfile.read())
 
-# count = 0
-# list_ids = []
-# for i in range(len(r.json())):
-#     list_ids.append(r.json()[i]['id'])
+    count = 0
+    list_ids = []
+    for i in range(len(r.json())):
+        list_ids.append(r.json()[i]['id'])
 
-# while list_ids[count] != int(first_row['id']):
-#     print(list_ids[count], int(first_row['id']))
-#     count += 1
+    while list_ids[count] != last_id:
+        print(list_ids[count], last_id)
+        count += 1
 
-# print(count)
+    print(count)
 
-# with open('news.csv', 'a', newline='') as csvfile:
-#     fieldnames = list(r.json()[0].keys())
-#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#     writer.writerows(r.json()[:count])
+    with open('news.csv', 'a', newline='') as csvfile:
+        fieldnames = list(r.json()[0].keys())
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerows(r.json()[:count][::-1])
+    
+    with open('last_id.txt', 'w') as txtfile:
+        txtfile.write(str(r.json()[0]['id']))
 
-with open('news.csv', 'w', newline='') as csvfile:
-    fieldnames = list(r.json()[0].keys())
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerows(r.json()[::-1])
+else:
+    with open('news.csv', 'w', newline='') as csvfile:
+        fieldnames = list(r.json()[0].keys())
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(r.json()[::-1])
+    
+    with open('last_id.txt', 'w') as txtfile:
+        txtfile.write(str(r.json()[0]['id']))
