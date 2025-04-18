15ac604a4c6ed3ce6b50415595aa1afa3c06002f
Fabio Dantas Fernandes <nunseik@gmail.com>
2025-04-18 13:06:59 -0300

    functions implemented

--- main.py
+++ main.py
@@ -2,41 +2,73 @@ import requests
 import csv
 import os
 
-r = requests.get('https://www2.itanhaem.sp.gov.br/wp-json/wp/v2/posts', timeout=1)
+#Constants
+fieldnames = ('id', 'modified', 'slug', 'content')
+url="https://www2.itanhaem.sp.gov.br/wp-json/wp/v2/posts?per_page=20&page=1"
 
-if r.status_code != requests.codes.ok:
-    raise Exception("Servidor indisponivel, tente novamente.")
+def get_url_data(url):
+    raw_data = requests.get(url, timeout=1)
 
-if os.path.exists("news.csv"):
+    if raw_data.status_code != requests.codes.ok:
+        raise Exception("Servidor indisponivel, tente novamente.")
+    
+    return raw_data
 
-    with open('last_id.txt', 'r') as txtfile:
-        last_id = int(txtfile.read())
+def find_new_entries(raw_data):
+    if os.path.exists("last_id.txt"):
+        with open('last_id.txt', 'r') as txtfile:
+            last_id = int(txtfile.read())
+    elif os.path.exists("news.csv"):
+        with open('news.csv', 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                last_id = int(row['id'])
+    else:
+        return []
 
     count = 0
     list_ids = []
-    for i in range(len(r.json())):
-        list_ids.append(r.json()[i]['id'])
+    for i in range(len(raw_data.json())):
+        list_ids.append(raw_data.json()[i]['id'])
 
     while list_ids[count] != last_id:
-        print(list_ids[count], last_id)
+        print(f"new id: {list_ids[count]}, last id: {last_id}")
         count += 1
 
-    print(count)
+    print(f"Found {count} new entries")
 
-    with open('news.csv', 'a', newline='') as csvfile:
-        fieldnames = list(r.json()[0].keys())
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writerows(r.json()[:count][::-1])
-    
-    with open('last_id.txt', 'w') as txtfile:
-        txtfile.write(str(r.json()[0]['id']))
+    return raw_data.json()[:count]
 
-else:
-    with open('news.csv', 'w', newline='') as csvfile:
-        fieldnames = list(r.json()[0].keys())
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(r.json()[::-1])
-    
+def create_last_id_file(raw_data):
     with open('last_id.txt', 'w') as txtfile:
-        txtfile.write(str(r.json()[0]['id']))
+            txtfile.write(str(raw_data.json()[0]['id']))
+
+def csv_creator(raw_data, new_entries, fieldnames):
+    if os.path.exists("news.csv"):
+
+        with open('news.csv', 'a', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, extrasaction='ignore')
+            writer.writerows(new_entries[::-1])
+
+    else:
+        with open('news.csv', 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, extrasaction='ignore')
+            writer.writeheader()
+            writer.writerows(raw_data.json()[::-1])
+        
+    create_last_id_file(raw_data)
+
+def get_new_entries_dict(new_entries, fieldnames):
+    new_entries_lst = []
+    for entry in new_entries:
+        new_entry_dict = {key: entry[key] for key in fieldnames}
+        new_entries_lst.append(new_entry_dict)
+    return new_entries_lst
+
+raw_data = get_url_data(url=url)
+new_entries = find_new_entries(raw_data)
+csv_creator(raw_data, new_entries, fieldnames)
+
+new_entries_lst = get_new_entries_dict(new_entries, fieldnames)
+for dict in new_entries_lst:
+    print(dict)
